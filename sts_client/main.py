cc0de77361ccf06f0a4da233ff16785db663f0b3
xxueewa <xueyanwapply@gmail.com>
2026-05-19 00:09:09 -0700

    update sts client

--- sts_client/main.py
+++ sts_client/main.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langsmith.middleware import TracingMiddleware
 
-from .sst_stream import record_until_silence
+from sst_stream import record_until_silence
 
 load_dotenv()
 
