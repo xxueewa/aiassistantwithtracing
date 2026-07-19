2aea0e57bd0976d7daaaa817ded4000d582b704c
xxueewa <xueyanwapply@gmail.com>
2026-07-18 21:19:02 -0700

    update rag tool

--- agentstudio/src/tools/rag_tool.py
+++ agentstudio/src/tools/rag_tool.py
@@ -10,5 +10,12 @@ def info_retrieval(query: str) -> str:
     retrieves domain info from db
     e.g. company employee profiles, meeting agenda, calendar info
     """
-    answer = run_rag_chain(query)
-    return answer["answer"]
+    import requests
+    url = "http://0.0.0.0:80/query"
+    response = requests.post(url, json={"question": query})
+
+    if response.status_code == 200:
+        answer = response.json()
+        return answer["answer"]
+
+    return {"error": "Could not fetch data", "status_code": response.status_code}
